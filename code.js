a5912089da248a83a3e78eb43c20a11daecf68c4
Ale Borge <clouix23@gmail.com>
2024-06-09 00:57:39 -0600

    card showing info and basic style

--- code.js
+++ code.js
@@ -1,6 +1,43 @@
+const myLibrary = [];
+const cardContainer = document.querySelector('.card-container');
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+
+function addBookToLibrary(Book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.textContent = `Title: ${Book.title}`;
+
+    const cardAuthor = document.createElement('p');
+    cardAuthor.textContent = `Author: ${Book.author}`;
+
+    const cardPages = document.createElement('p');
+    cardPages.textContent = `Pages: ${Book.pages}`;
+
+    const cardRead = document.createElement('p');
+    cardRead.textContent = `Read: ${Book.read}`;
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardRead);
+
+    cardContainer.appendChild(card);
+}
+
+const book1 = new Book("Test1", "ASSA", 123, false);
+const book2 = new Book("Test2", "SSA", 123, false);
+const book3 = new Book("Test3", "ASD", 123, true);
+myLibrary.push(book1, book2, book3);
+console.log(myLibrary)
+
+const book4 = new Book("Test3", "ASD", 123, true);
+
+addBookToLibrary(book1);
